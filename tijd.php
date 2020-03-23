4df0100972987040c8d1588be4d744c4e48a9f9c
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-23 14:12:52 +0100

    verbeter

--- tijd.php
+++ tijd.php
@@ -1,27 +1,27 @@
 <?php
 $totaal = 0;
 $set = explode(" ", $argv[1]);
-    foreach($set as $value){
-        $time_value = substr($value,-1);
+foreach($set as $value){
+        $time_value = substr($value, -1);
     switch($time_value){
-        case "s": 
+    case "s": 
             $antwoord = (int)$value;
             $totaal += $antwoord;
-        break;
-        case "m":
+         break;
+    case "m":
             $antwoord = (int)$value * 60;
             $totaal += $antwoord;
-        break;
-        case "h":
+         break;
+    case "h":
             $antwoord = (int)$value * 3600;
             $totaal += $antwoord;
-        break;
-        case "d":
+         break;
+    case "d":
             $antwoord = (int)$value * 86400;
             $totaal += $antwoord;
-        break;       
+         break;       
     }
-}
+ }
 echo $totaal . " seconden";
 
 ?>
